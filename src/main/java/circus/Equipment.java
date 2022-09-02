41357b44d86e42945fb19f85b20b7ec2074989a5
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2022-09-02 23:11:36 +0800

    Compile error

--- src/main/java/circus/Equipment.java
+++ src/main/java/circus/Equipment.java
@@ -1,10 +1,13 @@
-public abstract class Equipment {
+package circus;
+
+public abstract class Equipment implements Asset {
     protected int purchasePrice;
 
     public Equipment(int purchasePrice) {
         this.purchasePrice = purchasePrice;
     }
 
+    @Override
     public int getValue() {
         return purchasePrice;
     }
